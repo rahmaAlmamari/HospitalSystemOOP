c641c0c5e78e43fbdef6ba0cf30668e729f01311
rahmaAlmamari <alr816125@gmail.com>
2025-07-22 14:48:46 +0400

    Adding constructor for doctor and patient class which using person constructor

--- Doctor.cs
+++ Doctor.cs
@@ -8,6 +8,13 @@ namespace HospitalSystemOOP
 {
     class Doctor : Person
     {
-
+        //1. class Doctor fields ...
+        //2. class Doctor properties ...
+        //3. class Doctor methods ...
+        //4. class Doctor constructor ...
+        public Doctor(string name, int personAge) : base(name, personAge)
+        {
+            // Doctor specific initialization can go here if needed
+        }
     }
 }
--- Patient.cs
+++ Patient.cs
@@ -8,5 +8,13 @@ namespace HospitalSystemOOP
 {
     class Patient : Person
     {
+        //1. class Patient fields ...
+        //2. class Patient properties ...
+        //3. class Patient methods ...
+        //4. class Patient constructor ...
+        public Patient(string name, int personAge) : base(name, personAge)
+        {
+            // Patient specific initialization can go here if needed
+        }
     }
 }
