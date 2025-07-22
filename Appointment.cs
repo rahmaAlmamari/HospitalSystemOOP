37fbba39193e419b924ab1ab6756d0a665f2fd19
rahmaAlmamari <alr816125@gmail.com>
2025-07-22 15:39:20 +0400

    Adding feilds and constructor to Appointment class

--- Appointment.cs
+++ Appointment.cs
@@ -8,5 +8,19 @@ namespace HospitalSystemOOP
 {
     class Appointment
     {
+        //1. class Appointment fields ...
+        public static int AppointmentCount = 0; // Static field to count appointments
+        public int AppointmentID; // Unique ID for each appointment
+        Doctor AppointmentDoctor; // Doctor for the appointment
+        Patient AppointmentPatient; // Patient for the appointment
+        DateTime AppointmentDate; // Date and time of the appointment
+        //2. class Appointment properties ...
+        //3. class Appointment methods ...
+        //4. class Appointment constructor ...
+        public Appointment()
+        {
+            AppointmentCount++; // Increment the static count for appointments
+            AppointmentID = AppointmentCount; // Assign a unique ID to the appointment
+        }
     }
 }
