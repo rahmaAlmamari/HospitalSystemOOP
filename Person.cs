0ab4dfa44f8418188a95fbc942301aaf71eefa9c
rahmaAlmamari <alr816125@gmail.com>
2025-07-22 14:34:21 +0400

    Adding virtual DisplayInfo to preson class

--- Person.cs
+++ Person.cs
@@ -12,15 +12,21 @@ namespace HospitalSystemOOP
         public static int PersonCount = 0;
         public int PersonID;
         public string PersonName;
+        public int PersonAge;
 
         //2. class Person properties ...
         //3. class Person methods ... 
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"ID: {PersonID}, Name: {PersonName}, Age: {PersonAge}");
+        }
         //4. class Person constructor ...
-        public Person(string name)
+        public Person(string name , int personAge)
         {
             PersonCount++; // Increment the static count
             PersonID = PersonCount; // Assign the current count to the instance ID
             PersonName = name;
+            PersonAge = personAge;
         }
     }
 }
