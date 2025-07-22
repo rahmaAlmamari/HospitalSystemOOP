8a77daa39b38e3baa3033d6435536fdc728335f9
rahmaAlmamari <alr816125@gmail.com>
2025-07-22 14:30:19 +0400

    Adding feilds and constructor to person class

--- Person.cs
+++ Person.cs
@@ -8,5 +8,19 @@ namespace HospitalSystemOOP
 {
     class Person
     {
+        //1. class Person feilds ...
+        public static int PersonCount = 0;
+        public int PersonID;
+        public string PersonName;
+
+        //2. class Person properties ...
+        //3. class Person methods ... 
+        //4. class Person constructor ...
+        public Person(string name)
+        {
+            PersonCount++; // Increment the static count
+            PersonID = PersonCount; // Assign the current count to the instance ID
+            PersonName = name;
+        }
     }
 }
