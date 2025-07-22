f9a776235cf2991d934cfe2aed035491a74add08
rahmaAlmamari <alr816125@gmail.com>
2025-07-22 15:24:31 +0400

    Creating Hospital class and adding HospitalDoctors list and HospitalPatients list

---
+++ Hospital.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalSystemOOP
+{
+    class Hospital
+    {
+        //1. HospitalDoctors list ...
+        public static List<Doctor> HospitalDoctors = new List<Doctor>();
+        //2. HospitalPatients list ...
+        public static List<Patient> HospitalPatients = new List<Patient>();
+    }
+}
