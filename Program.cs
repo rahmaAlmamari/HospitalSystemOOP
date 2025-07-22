f64480c6979e52c2e6574d6b79c425f201744be7
rahmaAlmamari <alr816125@gmail.com>
2025-07-22 15:15:24 +0400

    Adding HospitalDoctors and HospitalPatients list

--- Program.cs
+++ Program.cs
@@ -2,6 +2,10 @@
 {
     internal class Program
     {
+        //1. HospitalDoctors list ...
+        public static List<Doctor> HospitalDoctors = new List<Doctor>();
+        //2. HospitalPatients list ...
+        public static List<Patient> HospitalPatients = new List<Patient>();
         static void Main(string[] args)
         {
             
