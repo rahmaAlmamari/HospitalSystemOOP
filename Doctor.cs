4955477ac4843338653536051fdd1b5fc9a5fe94
rahmaAlmamari <alr816125@gmail.com>
2025-07-23 08:00:19 +0400

    Creating GetDoctor and AddDoctors method

--- Doctor.cs
+++ Doctor.cs
@@ -23,6 +23,48 @@ namespace HospitalSystemOOP
                               $"Doctor Specialization: {Specialization}\n" +
                               $"-------------------------------------------");
         }
+        //to GetPatients ...
+        public static bool GetDoctor()
+        {
+            //to check if there are patient register in the system or not ...
+            if (Hospital.HospitalDoctors.Count == 0)
+            {
+                Console.WriteLine("No doctors register in the system yet.");
+                return false;
+            }
+            return true;
+        }
+        //to AddPatient ...
+        public static void AddDoctors()
+        {
+            //to create a new doctor object ...
+            Doctor newDoctor = new Doctor(Validation.StringNamingValidation("doctor name"),//to get and send the doctor name ...
+                                             Validation.IntValidation("doctor age"));//to get and send the doctor age ...
+            //to get the doctor specialization ...
+            newDoctor.Specialization = Validation.StringValidation("doctor specialization");
+            //to get the doctor available appointments ...
+            bool flagAvailableAppointments = false; // Flag to control the loop of getting the available appointments ...
+            do 
+            {
+                newDoctor.AvailableAppointments.Add(Validation.DateTimeValidation("doctor available appointments"));
+                Console.WriteLine("Do you want to add another available appointment? (Y/N)");
+                char choice = Validation.CharValidation("available appointment");
+                if (choice == 'Y' || choice == 'y')
+                {
+                    flagAvailableAppointments = true; // Continue adding appointments
+                }
+                else
+                {
+                    flagAvailableAppointments = false; // Stop adding appointments
+                }
+            } while (flagAvailableAppointments);
+
+            //to store the newDoctor to HospitalDoctors list ...
+            Hospital.HospitalDoctors.Add(newDoctor);
+            Console.WriteLine("Doctor add successfully with following details:");
+            Console.WriteLine(newDoctor.DisplayInfo());
+            Additional.HoldScreen();//to hold the screen ...
+        }
         //4. class Doctor constructor ...
         public Doctor(string name, int personAge) : base(name, personAge)
         {
